add9e878fa171ef04be24d12fbe77946b83425c6
LukaLondaridzen <tactusglacies@gmail.com>
2022-09-05 00:41:36 +0400

    commit message

---
+++ socialnetwork/social/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.views import View
+from .models import Post
+
+
+class PostListView(View):
+    def get(self, request, *args, **kwargs):
+        posts = Post.objects.all().order_by('-created_on')
+
+        context = {
+            'post_list': posts,
+
+        }
+        x = 'asd'
+        return render(request, 'social/post_list.html', context)
